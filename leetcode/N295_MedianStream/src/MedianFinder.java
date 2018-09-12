6fc1c9f8016e45312eed8a01534356304dd8ba56
Stefan Gloutnikov <stefan@gloutnikov.com>
2018-09-11 23:37:10 -0700

    init problem...

---
+++ leetcode/N295_MedianStream/src/MedianFinder.java
@@ -0,0 +1,26 @@
+class MedianFinder {
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
